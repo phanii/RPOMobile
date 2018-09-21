aa1b3ae665a28044e35797e2f0cfa8dd540ae6be
Phani Kumar G <514521+phanii@users.noreply.github.com>
2018-09-21 17:56:22 +0530

    added login validation

--- app/src/main/java/com/rpo/mobile/login/Login.kt
+++ app/src/main/java/com/rpo/mobile/login/Login.kt
@@ -2,14 +2,17 @@ package com.rpo.mobile.login
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
 import com.mindorks.editdrawabletext.DrawablePosition
 import com.mindorks.editdrawabletext.onDrawableClickListener
 import com.rpo.mobile.R
 import com.rpo.mobile.utils.Utils
 import kotlinx.android.synthetic.main.activity_login.*
 
-class Login : AppCompatActivity() {
 
+class Login : AppCompatActivity() {
+    val TAG = Login::class.java.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -24,7 +27,51 @@ class Login : AppCompatActivity() {
             }
 
         })
+        btn_user_login.setOnClickListener { performLogin() }
     }
 
+    private fun performLogin() {
+        if (validate(username.text.toString(), password.text.toString())) {
+            /* error_message.text = "Username or Password is incorrect,\n" +
+                     "please try again."
+             error_message.visibility = View.VISIBLE
+             Log.d(TAG, "${error_message.text.toString()}: ")*/
+            error_message.visibility = View.GONE
+            Toast.makeText(this, "login success", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    private fun validate(email: String, password: String): Boolean {
+
+        // Reset errors.
+
+        if (email.isEmpty() && password.isEmpty()) {
+            error_message.text = "Username or Password is incorrect,\n" +
+                    "please try again."
+            error_message.visibility = View.VISIBLE
+            return false
+        } else if (email.isEmpty()) {
+            error_message.text = "Email is required"
+            error_message.visibility = View.VISIBLE
+            return false
+        } else if (!Utils().isEmailValid(username.text.toString())) {
+            error_message.text = "Enter a valid email"
+            error_message.visibility = View.VISIBLE
+            return false
+        }
+
+        if (password.isEmpty()) {
+            error_message.text = "Password is required"
+            error_message.visibility = View.VISIBLE
+            return false
+        } else if (!Utils().isPasswordValid(password)) {
+            error_message.text = "Password must contain at least 6 characters"
+            error_message.visibility = View.VISIBLE
+            return false
+        }
+
+        return true
+    }
 
 }
--- app/src/main/java/com/rpo/mobile/utils/Utils.kt
+++ app/src/main/java/com/rpo/mobile/utils/Utils.kt
@@ -1,6 +1,7 @@
 package com.rpo.mobile.utils
 
 import android.content.Context
+import android.util.Patterns
 import android.widget.Toast
 
 class Utils {
@@ -12,4 +13,14 @@ class Utils {
         return Toast.makeText(context, this.toString(), duration).apply { show() }
     }
 
+    fun isEmailValid(email: String): Boolean {
+        val pattern = Patterns.EMAIL_ADDRESS
+        val matcher = pattern.matcher(email)
+        return matcher.matches()
+    }
+
+    //Check password with minimum requirement here(it should be minimum 6 characters)
+    fun isPasswordValid(password: String): Boolean {
+        return password.length >= 6
+    }
 }
